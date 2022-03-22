f5f83a7a7e6ebbabe4086391fa8fae9bfc75bddb
vyxerG <gregoryogumerem@gmail.com>
2022-03-22 04:13:57 +0100

    Added new Files

--- TodoList/account/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
-
-    def ready(self):
-        import account.signals  # Tells django to search inside our usersApp then check inside the submodule by name signals it will then see the Handlers
