b03ab342f2892b1fe973f3a69422e44c457b7df0
vyxerG <gregoryogumerem@gmail.com>
2022-03-22 03:23:10 +0100

    Thing to do

---
+++ TodoList/static/js/script.js
@@ -0,0 +1,12 @@
+
+
+const close = document.querySelector('.alert__close');
+
+const activate_close = document.querySelector('.card_c');
+
+
+close.addEventListener('click', function(){
+    activate_close.style.display = 'none';
+});
+
+
